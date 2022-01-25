322e79240c218717ae03c527c4fb7373e80ebf65
shults.b <schulz150701@gmail.com>
2022-01-25 15:20:32 +0200

    Reworked 2 Selenium tests with usage of Specflow

---
+++ SpecFlowPractice - Reworked/Steps/CartSteps.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using SpecFlowPractice.Drivers;
+using SpecFlowPractice.Pages;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowPractice.Steps
+{
+    [Binding]
+    class CartSteps
+    {
+        public WebDriverSettings Driver { get; }
+        private ProductPageObject SearchResult = new();
+        private BasePageObject BasePage = new();
+        private List<string> InfoAboutProductOnProductList = new();
+        private List<string> InfoAboutProductOnCart = new();
+
+        public CartSteps(WebDriverSettings driver)
+        {
+            this.Driver = driver;
+        }
+
+        [Given(@"User entered to search bar text '(.*)' and pressed button enter, after that filtering for expensive first had been chosen")]
+        public void GivenUserEnteredToSearchBarTextAndPressedButtonEnterAfterThatFilteringForExpensiveFirstHadBeenChosen(string searchRequest)
+        {
+            BasePage.SearchBar(searchRequest);
+            SearchResult.Sorting();
+        }
+
+        [When(@"User adds product to the cart and opens cart")]
+        public void WhenUserAddsProductToTheCartAndOpensCart()
+        {
+            int productNumber = 1;
+            InfoAboutProductOnProductList = SearchResult.AddProductToCart(productNumber);
+            SearchResult.OpenCart();
+        }
+
+        [Then(@"Price of product in the cart must be same as price at the product list")]
+        public void ThenPriceOfProductInTheCartMustBeSameAsPriceAtTheProductList()
+        {
+            InfoAboutProductOnCart = SearchResult.InfoAboutProductAlreadyAddedToTheCart();
+            InfoAboutProductOnProductList.Should().BeEquivalentTo(InfoAboutProductOnCart);
+        }
+    }
+}
---
+++ SpecFlowPractice - Reworked/Variables/ConfigSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowPractice.Variables
+{
+    class ConfigSettings
+    {
+        public string BrowserType { get; set; }
+        public string BaseUrl { get; set; }
+    }
+}
