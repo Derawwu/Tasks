8384d8310f33fa46919f0e63d0f925dd4f3ce522
shults.b <schulz150701@gmail.com>
2022-01-28 17:27:29 +0200

    Database tests

---
+++ EntittyFrameworkCoreTests/Helpers/GetCustomerQuerry.cs
@@ -0,0 +1,27 @@
+﻿using EntittyFrameworkCoreTests.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntittyFrameworkCoreTests.Helpers
+{
+    internal class GetCustomerQuerry
+    {
+        private readonly NorthwindContext _context;
+
+        public GetCustomerQuerry(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Customers> Execute()
+        {
+            return _context.Customers
+                                    .OrderBy(c => c.CompanyName)
+                                    .ToList();
+        }
+    }
+}
---
+++ EntittyFrameworkCoreTests/UnitTest1.cs
@@ -0,0 +1,111 @@
+using EntittyFrameworkCoreTests.Helpers;
+using EntittyFrameworkCoreTests.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace EntittyFrameworkCoreTests
+{
+    public class Tests
+    {
+        Random rand = new Random();
+
+        public DbContextOptions<NorthwindContext> dbContextOptions;
+
+        public DbContextOptionsBuilder<NorthwindContext> dbContextOptionsBuilder;
+
+        NorthwindContext _context;
+
+        private DbContextOptionsBuilder fer { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<NorthwindContext>()
+            .UseInMemoryDatabase(databaseName: "Northwind" + Convert.ToString(rand.NextDouble()))
+            .Options;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            _context = new NorthwindContext(dbContextOptions);
+
+             Seed(_context);
+
+            var querry = new GetCustomerQuerry(_context);
+
+            var result = querry.Execute();
+
+            Assert.AreEqual(5, result.Count);
+        }
+        [Test]
+        public void Test2()
+        {
+            _context = new NorthwindContext(dbContextOptions);
+
+            Seed(_context);
+
+            var querry = new GetCustomerQuerry(_context);
+
+            var result = querry.Execute();
+
+            Assert.AreEqual("Global Logic", result.First().CompanyName);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            _context = new NorthwindContext(dbContextOptions);
+
+            Seed(_context);
+
+            var querry = new GetCustomerQuerry(_context);
+
+            var result = querry.Execute();
+
+            Assert.AreEqual(result.Count, result.Distinct().Count());
+        }
+
+        [Test]
+        public void Test4()
+        {
+            _context = new NorthwindContext(dbContextOptions);
+
+            Seed(_context);
+
+            var querry = new GetCustomerQuerry(_context);
+
+            var result = querry.Execute();
+
+            Assert.NotNull(result);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+
+        private void Seed(NorthwindContext context) 
+        {
+
+            var customers = new[]
+            {
+                new Customers {CustomerId = "N",CompanyName = "Nix" },
+                new Customers {CustomerId = "P",CompanyName = "PX" },
+                new Customers {CustomerId = "S",CompanyName = "SoftServe" },
+                new Customers {CustomerId = "G",CompanyName = "Global Logic" },
+                new Customers {CustomerId = "U",CompanyName = "Udemy" },
+            };
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
+
+        
+
+    }
+}
